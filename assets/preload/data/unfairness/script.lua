8175927b4a3e8e8870daa7d4f970b15e91b2b014
KFC-Chicken-1212 <89538185+KFC-Chicken-1212@users.noreply.github.com>
2022-03-18 23:41:26 -0400

    added the file lpl

---
+++ assets/preload/data/unfairness/script.lua
@@ -0,0 +1,49 @@
+function opponentNoteHit() 
+	curHealth = getProperty('health')
+	if curHealth >= 0.1 then
+	addHealth(-0.01)
+	end
+end
+
+function onStartCountdown()
+setProperty('health', 2)
+end
+
+function onUpdate(elapsed)
+songPos = getSongPosition()
+local currentBeat = (songPos/5000)*(curBpm/60)
+setCharacterY('dad',getCharacterY('dad') + (math.sin(currentBeat) * 0.6))
+function onMoveCamera(focus)
+	if focus == 'boyfriend' then
+		-- called when the camera focus on boyfriend
+	elseif focus == 'dad' then
+		setProperty('camFollowPos.y',getProperty('camFollowPos.y') + (math.sin(currentBeat) * 0.6))
+	end
+end
+if difficulty ~= 0 then
+	noteTweenX(defaultPlayerStrumX0, 4, ((screenWidth / 2) - (157 / 2)) + (math.sin((currentBeat) + 0) * 300), 0.001)
+	noteTweenX(defaultPlayerStrumX1, 5, ((screenWidth / 2) - (157 / 2)) + (math.sin((currentBeat) + 1) * 300), 0.001)
+	noteTweenX(defaultPlayerStrumX2, 6, ((screenWidth / 2) - (157 / 2)) + (math.sin((currentBeat) + 2) * 300), 0.001)
+	noteTweenX(defaultPlayerStrumX3, 7, ((screenWidth / 2) - (157 / 2)) + (math.sin((currentBeat) + 3) * 300), 0.001)
+	noteTweenY('defaultPlayerStrumY0', 4, ((screenHeight / 2) - (157 / 2)) + (math.cos((currentBeat) + 0) * 300), 0.001)
+	noteTweenY('defaultPlayerStrumY1', 5, ((screenHeight / 2) - (157 / 2)) + (math.cos((currentBeat) + 1) * 300), 0.001)
+	noteTweenY('defaultPlayerStrumY2', 6, ((screenHeight / 2) - (157 / 2)) + (math.cos((currentBeat) + 2) * 300), 0.001)
+	noteTweenY('defaultPlayerStrumY3', 7, ((screenHeight / 2) - (157 / 2)) + (math.cos((currentBeat) + 3) * 300), 0.001)
+	noteTweenX('fake1', 0, ((screenWidth / 2) - (157 / 2)) + (math.sin((currentBeat) + (4) * 2) * 300), 0.001)
+	noteTweenX('fake2', 1, ((screenWidth / 2) - (157 / 2)) + (math.sin((currentBeat) + (5) * 2) * 300), 0.001)
+	noteTweenX('fake3', 2, ((screenWidth / 2) - (157 / 2)) + (math.sin((currentBeat) + (6) * 2) * 300), 0.001)
+	noteTweenX('fake4', 3, ((screenWidth / 2) - (157 / 2)) + (math.sin((currentBeat) + (7) * 2) * 300), 0.001)
+	noteTweenY('defaultFPlayerStrumY0', 0, ((screenHeight / 2) - (157 / 2)) + (math.cos((currentBeat) + (4) * 2) * 300), 0.001)
+	noteTweenY('defaultFPlayerStrumY1', 1, ((screenHeight / 2) - (157 / 2)) + (math.cos((currentBeat) + (5) * 2) * 300), 0.001)
+	noteTweenY('defaultFPlayerStrumY2', 2, ((screenHeight / 2) - (157 / 2)) + (math.cos((currentBeat) + (6) * 2) * 300), 0.001)
+	noteTweenY('defaultFPlayerStrumY3', 3, ((screenHeight / 2) - (157 / 2)) + (math.cos((currentBeat) + (7) * 2) * 300), 0.001)
+	end
+end
+
+function onCreatePost()
+
+    makeLuaSprite('void', 'void', -210, 0) 
+    addLuaSprite('void')
+    addGlitchEffect('void', 2, 2)
+
+end
---
+++ assets/preload/stages/void.lua
@@ -0,0 +1,8 @@
+function onCreate()
+	makeLuaSprite('void', 'void', -1200, -600);
+	addLuaSprite('void',false)
+	setLuaSpriteScrollFactor('void', 0.1, 0.1);
+	scaleObject('void', 1.6, 1.6);
+	addGlitchEffect('void', 2,2)
+	addLuaSprite('void')
+end
